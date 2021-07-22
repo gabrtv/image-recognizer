d6c90bc83d1dd2ea2008d3fcce13d4c7bca35fd7
Gabe Monroy <gabe.monroy@microsoft.com>
2021-07-22 11:53:04 -0600

    Add save functionality to Cosmos MongoDB database

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ subscription_key = os.environ.get("AZURE_COMPVISION_KEY")
 endpoint = os.environ.get("AZURE_COMPVISION_ENDPOINT")
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
 
+# auth to cosmosdb
+import pymongo
+cosmos_dbname = os.environ["AZURE_COSMOS_DBNAME"]
+cosmos_colname = os.environ["AZURE_COSMOS_COLNAME"]
+cosmos_uri = os.environ["AZURE_COSMOS_CONNSTRING"]
+cosmos_client = pymongo.MongoClient(cosmos_uri)
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -20,33 +27,42 @@ def get_image_tags(remote_image_url):
     tags_result_remote = computervision_client.tag_image(remote_image_url)
 
     if (len(tags_result_remote.tags) == 0):
-        return "No tags detected."
+        return []
     else:
-        tags_detected = []
-        
-        for tag in tags_result_remote.tags:
-            t = "'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100)
-            tags_detected.append(t)
+        return tags_result_remote.tags
 
-        return tags_detected
-
-def response_builder(result):
+def response_builder(tags):
     response = """
     <html>
     <title>Image Recognizer</title>
     """
-    response += "<ul>\n"
-    response += "<p>Tags detected in image</p>"
-    for item in result:
-        response += "<li>{}</li>\n".format(item)
-    response += "</ul>"
+    if len(tags) == 0:
+        response += "<p>No tags detected</p>"
+    else:
+        response += "<p>Tags detected in image</p>"
+        response += "<ul>\n"
+        for tag in tags:
+            t = "'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100)
+            response += "<li>{}</li>\n".format(t)
+        response += "</ul>"
     response += "</html>"
     return response
+
+def save_image_tags(tags):
+    db = cosmos_client.get_database(cosmos_dbname)
+    col = db.get_collection(cosmos_colname)
+    doc = {'tags': {}}
+    for tag in tags:
+        doc['tags'][tag.name] = tag.confidence * 100
+    result = col.insert_one(doc)
+    print("Inserted to database: {}".format(result.inserted_id))
 
 @app.route("/test")
 def tag_test():
     remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
     tags = get_image_tags(remote_image_url)
+    if tags:
+        save_image_tags(tags)
     response = response_builder(tags)
     return response
 
